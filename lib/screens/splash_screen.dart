a609beef7310c880902d9d040ae64e69c05ca83c
sabahat-12345 <sabahatali976@gmail.com>
2025-04-05 17:06:09 +0500

    Added splash screen, login screen, and forget password screen UI

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,51 @@
+// import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:grocery_plus/screens/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  @override
+  void initState() {
+    super.initState();
+    manageSession();
+  }
+
+  manageSession() async {
+    await Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushAndRemoveUntil(context,
+          MaterialPageRoute(builder: (c) => LoginScreen()), (route) => false);
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              "images/splash_image.png",
+              height: 100,
+              width: 200,
+            ),
+          ),
+          Text(
+            "Grocery Plus",
+            style:
+                GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.w500),
+          ),
+        ],
+      ),
+    );
+  }
+}
